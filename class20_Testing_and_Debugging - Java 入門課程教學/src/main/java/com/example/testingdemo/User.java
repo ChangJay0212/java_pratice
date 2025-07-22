214592252b0613e35b9fedfd5ddf1d6e704fed58
ChangJay0212 <79964032+ChangJay0212@users.noreply.github.com>
2025-07-22 16:03:30 +0800

    fix: the user is used by default

--- class20_Testing_and_Debugging - Java 入門課程教學/src/main/java/com/example/testingdemo/User.java
+++ class20_Testing_and_Debugging - Java 入門課程教學/src/main/java/com/example/testingdemo/User.java
@@ -1,11 +1,12 @@
 package com.example.testingdemo;
-
+import javax.persistence.Table;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
